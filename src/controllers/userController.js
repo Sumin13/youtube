7ef533839af171bc7cedfa0f71aaf681dc4d11ba
Sumin13 <su184913@icloud.com>
2024-02-25 22:14:00 +0000

    25/02/24

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/User";
+import Video from "../models/Video";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
+import { render } from "pug";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -195,4 +197,16 @@ export const postChangePassword = async (req, res) => {
   await user.save();
   return res.redirect("/users/logout");
 };
-export const see = (req, res) => res.send("See User");
+export const see = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id).populate("videos");
+  if (!user) {
+    return res.status(404) / render("404", { pageTitle: "User not found" });
+  }
+  const videos = await Video.find({ owner: user._id });
+  return res.render("users/profile", {
+    pageTitle: user.name,
+    user,
+    videos,
+  });
+};
